20f93fc8ec0df4eaa02d28909dc08c2b99c1f7c1
LoicPeter <loic.peter@gmail.com>
2021-03-29 21:06:45 +0200

    Added license terms

--- include/landmark_annotator.hpp
+++ include/landmark_annotator.hpp
@@ -1,3 +1,19 @@
+// -------------------------------------------------------------------
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// -------------------------------------------------------------------
+
+
 #ifndef LANDMARK_ANNOTATOR_H_INCLUDED
 #define LANDMARK_ANNOTATOR_H_INCLUDED
 
@@ -7,8 +23,6 @@
 #include <iostream>
 #include <set>
 #include <map>
-//#include "graph.hpp"
-//#include "utilities.hpp"
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 #include <opencv2/core/core.hpp>
